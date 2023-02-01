b1fedd557acbc8ebe220da07e192b2e47db1e7f7
DanielaBLM <60104195+DanielaBLM@users.noreply.github.com>
2023-02-01 18:14:37 +0100

    Update Collection.java

--- Collection.java
+++ Collection.java
@@ -1,6 +1,6 @@
 package MyAdapter;
 
-import java.util.Vector;
+import java.util.*;
 
 public class Collection implements HCollection{
 
@@ -13,16 +13,28 @@ public class Collection implements HCollection{
 	
 	@Override
 	public boolean add(Object o) throws NullPointerException  {
-		if(o.equals(null)) {
-			throw new NullPointerException();
+		if(o.equals(null)) throw new NullPointerException();
+		else{
+			vettore.addElement(o);
+			return true;
 		}
-		vettore.addElement(o);
-		return true;
+		
 	}
 
 	@Override
 	public boolean addAll(HCollection c) {
-		return false;
+		Object temp;
+		HIterator it = c.iterator();
+		if(c.isEmpty()) throw new NullPointerException();
+		while (it.hasNext()){
+			temp = it.next();
+			if(temp.equals(null)) throw new NullPointerException();
+			else {
+				add(temp);
+			}
+		}
+		
+		return true;
 	}
 
 	@Override
@@ -40,8 +52,18 @@ public class Collection implements HCollection{
 
 	@Override
 	public boolean containsAll(HCollection c) {
-		// TODO Auto-generated method stub
-		return false;
+		Object temp;
+		HIterator it = c.iterator();
+		if(c.isEmpty()) throw new NullPointerException();
+		while (it.hasNext()){
+			temp = it.next();
+			if(temp.equals(null)) throw new NullPointerException();
+			if(!contains(temp)) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 	@Override
@@ -51,25 +73,47 @@ public class Collection implements HCollection{
 
 	@Override
 	public HIterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		HIterator it = new Iterator();
+		return it;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		return vettore.removeElement(o);
+		if(o.equals(null)) throw new NullPointerException();
+	    else {
+	    	if(!contains(o)) return false;
+	    	return vettore.removeElement(o);
+	    }
 	}
 
 	@Override
 	public boolean removeAll(HCollection c) {
-		// TODO Auto-generated method stub
-		return false;
+		Object temp;
+		HIterator it = c.iterator();
+		if(c.isEmpty()) throw new NullPointerException();
+		while (it.hasNext()){
+			temp = it.next();
+			if(temp.equals(null)) throw new NullPointerException();
+			if(contains(temp)) {
+				remove(temp);
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public boolean retainAll(HCollection c) {
-		// TODO Auto-generated method stub
-		return false;
+		Object temp;
+		HIterator it = c.iterator();
+		if(c.isEmpty()) throw new NullPointerException();
+		while (it.hasNext()){
+			temp = it.next();
+			if(temp.equals(null)) throw new NullPointerException();
+			if(!contains(temp)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
@@ -88,4 +132,76 @@ public class Collection implements HCollection{
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
+	
+	  //HITERATOR
+
+	private class Iterator implements HIterator {
+
+	    private int startIndex;
+	    private int currentIndex;
+	    private int endIndex;
+	
+	    public Iterator() {
+	      startIndex = currentIndex = -1;
+	      endIndex = size() - 1;
+	    }
+	
+	    public Iterator(int from, int to) {
+	      if(from < 0) startIndex = currentIndex = -1;
+	      else startIndex = currentIndex = from - 1;
+	      if(to > (size() - 1)) endIndex = size() - 1;
+	      else endIndex = to;
+	    }
+	
+	    private int getStartIndex() {
+	      return startIndex;
+	    }
+	
+	    private int getCurrentIndex() {
+	      return currentIndex;
+	    }
+	
+	    private int goBackIndex() {
+	      return --currentIndex;
+	    }
+	
+	    public boolean hasNext() {
+	      return (currentIndex < endIndex);
+	    }
+	
+	    public Object next() throws NoSuchElementException {
+	      try {
+	        if(hasNext()) return getIterator(++currentIndex);
+	        else throw new NoSuchElementException();
+	      } catch(NoSuchElementException e) {
+	        System.out.println("There is no other object: " + e);
+	        return null;
+	      } catch(Exception e) {
+	        System.out.println(e);
+	        return null;
+	      }
+	    }
+	
+	    public void remove() throws IllegalStateException {
+	      try {
+	        if(currentIndex < 0) throw new IllegalStateException();
+	        else Collection.this.remove(currentIndex);
+	      } catch (IllegalStateException e) {
+	        System.out.println("next() method has not been called yet: " + e);
+	      } catch (Exception e) {
+	        System.out.println(e);
+	      }
+	      
+	    }
+	    
+	  }
+	  
+	private Object getIterator(int index) {
+	    return vettore.elementAt(index);
+	  }
+	
+	
+	
 }
