481f05b0b71ca3585e7afaa455c7e41072f8e607
DanielaBLM <60104195+DanielaBLM@users.noreply.github.com>
2023-02-01 12:31:37 +0100

    Update HSet.java

--- HSet.java
+++ HSet.java
@@ -8,22 +8,22 @@ public interface HSet extends HCollection{
 	//Adds all of the elements in the specified collection to this set if they're not already present (optional operation).
 	boolean addAll(HCollection c);
    
-	//Removes all of the elements from this collection (optional operation).
+	//Removes all of the elements from this set (optional operation).
 	void clear();
    
-	//Returns true if this collection contains the specified element.
+	//Returns true if this set contains the specified element.
 	boolean contains(Object o);
     
-	//Returns true if this collection contains all of the elements in the specified collection.
+	//Returns true if this set contains all of the elements in the specified collection.
 	boolean containsAll(HCollection c);
 	
-	//Compares the specified object with this collection for equality.
+	//Compares the specified object with this set for equality.
 	boolean equals(Object o);
    
-	//Returns the hash code value for this collection.
+	//Returns the hash code value for this set.
 	int hashCode();
 	
-	//Returns true if this collection contains no elements.
+	//Returns true if this set contains no elements.
 	boolean isEmpty();
    
 	//Returns an iterator over the elements in this collection.
@@ -32,19 +32,19 @@ public interface HSet extends HCollection{
 	//Removes the specified element from this set if it is present (optional operation).
 	boolean remove(Object o);
    
-	//Removes all this collection's elements that are also contained in the specified collection (optional operation).
+	//Removes all this set's elements that are also contained in the specified collection (optional operation).
 	boolean removeAll(HCollection c);
    
-	//Retains only the elements in this collection that are contained in the specified collection (optional operation).
+	//Retains only the elements in this set that are contained in the specified collection (optional operation).
 	boolean retainAll(HCollection c);
   
-	//Returns the number of elements in this collection.
+	//Returns the number of elements in this set.
 	int size();
 	
-	//Returns an array containing all of the elements in this collection.
-    	Object[] 	toArray();
+	//Returns an array containing all of the elements in this set.
+    	Object[] toArray();
    
-   	//Returns an array containing all of the elements in this collection; the runtime type of the returned array is that of the specified arr
+   	//Returns an array containing all of the elements in this set; the runtime type of the returned array is that of the specified array
    	 Object[] toArray(Object[] a);
 
 }
